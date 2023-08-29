5c899514e4a1e56684a032d207a367613a565283
Alac1996 <alacarno@gmail.com>
2023-08-29 16:17:46 +0700

    UpdateToDoList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import "./App.scss";
+import { FaHome } from "react-icons/fa";
 function App() {
-  return <div>React-App</div>;
+  return (
+    <div className="todo">
+      <div className="todo__header">
+        <header className="header">
+          <div className="header__logo">
+            <FaHome />
+          </div>
+          <div className="header__text">
+            <h1>TodoList</h1>
+          </div>
+          <div className="header__search">
+            <input />
+          </div>
+        </header>
+      </div>
+      <div className="todo__sidebar">SideBar</div>
+      <div className="todo_content">TodoContent</div>
+    </div>
+  );
 }
 
 export default App;
