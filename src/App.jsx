d16cc77fd03c09e15c042b6a2ec1686d9b541622
Alac1996 <alacarno@gmail.com>
2023-08-30 11:54:26 +0700

    Update2ToDoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.scss";
 // import { FaHome } from "react-icons/fa";
 import Header from "./Header";
+import {
+  FaInbox,
+  FaCalendar,
+  FaCalendarAlt,
+  FaChevronDown,
+} from "react-icons/fa";
 
 function App() {
   return (
@@ -8,7 +14,30 @@ function App() {
       <div className="todo__header">
         <Header />
       </div>
-      <div className="todo__sidebar">SideBar</div>
+      <div className="todo__sidebar">
+        <aside className="Sidebar">
+          <section className="sidebar__category">
+            <ul className="list">
+              <li className="list__item">
+                <FaInbox className="list__icon__list" />
+                <p className="list__item__text">Inbox</p>
+              </li>
+              <li className="list__item">
+                <FaInbox className="list__icon__list" />
+                <p className="list__item__text">Inbox</p>
+              </li>
+              <li className="list__item">
+                <FaInbox className="list__icon__list" />
+                <p className="list__item__text">Inbox</p>
+              </li>
+              <li className="list__item">
+                <FaInbox className="list__icon__list" />
+                <p className="list__item__text">Inbox</p>
+              </li>
+            </ul>
+          </section>
+        </aside>
+      </div>
       <div className="todo__content">TodoContent</div>
     </div>
   );
